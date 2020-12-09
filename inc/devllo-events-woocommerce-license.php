7e70f80c9d804f2e2b328b730802bdcff73cf588
Femi <94sam.fem@gmail.com>
2020-12-10 00:34:50 +0200

    License key

--- inc/devllo-events-woocommerce-license.php
+++ inc/devllo-events-woocommerce-license.php
@@ -4,14 +4,17 @@ if( !class_exists('WPUpdatrPlugins') ){
     require_once plugin_dir_path( __FILE__ ).'class.plugin-wp-updatr-products.php';
   }
 
+  $license_key = '';
+  $product_key = '';
+
   use WPUpdatrPlugins as devlloEventsWoocommerce;
 
   new devlloEventsWoocommerce\WPUpdatrPlugins( $license_key, $product_key );
 
   // License Key
 
-  new myPluginAlias\WPUpdatrPlugins( get_option( 'devlloEventsWoocommerce_api_key' ), $product_key );
+  new devlloEventsWoocommerce\WPUpdatrPlugins( get_option( 'devlloEventsWoocommerce_api_key' ), $product_key );
 
   // Plugin Product Key
-  new myPluginAlias\WPUpdatrPlugins( get_option( 'devlloEventsWoocommerce_api_key' ), 'ELP-d967699a40bdaef97ef84c7f32d0d1' );
+  new devlloEventsWoocommerce\WPUpdatrPlugins( get_option( 'devlloEventsWoocommerce_api_key' ), 'ELP-d967699a40bdaef97ef84c7f32d0d1' );
 
--- inc/devllo-events-woocommerce-settings.php
+++ inc/devllo-events-woocommerce-settings.php
@@ -7,3 +7,6 @@ add_action('devllo_events_admin_menu_item', 'devllo_events_woocommerce_menu_item
 function devllo_events_woocommerce_menu_item(){
     add_submenu_page( 'edit.php?post_type=devllo_event', __('WC Integratiom', 'devllo-events-woocommerce'), __('WC Integration', 'devllo-events-woocommerce'), 'manage_options', 'devllo-events-woocommerce-dashboard', 'devllo_events_woocommerce_settings'  ); 
 }
+?>
+
+<h1>License Key</h1>

be4bc760c182a1a4baea9254751220e3fb81d82f
Femi <94sam.fem@gmail.com>
2020-12-12 18:45:18 +0200

    Update devllo-events-woocommerce-license.php

--- inc/devllo-events-woocommerce-license.php
+++ inc/devllo-events-woocommerce-license.php
@@ -11,10 +11,11 @@ if( !class_exists('WPUpdatrPlugins') ){
 
   new devlloEventsWoocommerce\WPUpdatrPlugins( $license_key, $product_key );
 
-  // License Key
 /*
-  new devlloEventsWoocommerce\WPUpdatrPlugins( get_option( 'devllo-wc-license-key' ), $product_key );
+  // License Key
+    
+new devlloEventsWoocommerce\WPUpdatrPlugins( get_option( 'devllo-wc-license-key' ), $product_key );
 
   // Plugin Product Key
-  new devlloEventsWoocommerce\WPUpdatrPlugins( get_option( 'devllo-wc-license-key' ), 'ELP-d967699a40bdaef97ef84c7f32d0d1' );
+new devlloEventsWoocommerce\WPUpdatrPlugins( get_option( 'devllo-wc-license-key' ), 'ELP-d967699a40bdaef97ef84c7f32d0d1' );
 

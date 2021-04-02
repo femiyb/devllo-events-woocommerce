783011933bb662e5c9323220b65e8eefc2322458
devlloplugin <70232551+devlloplugin@users.noreply.github.com>
2021-04-02 17:18:05 +0200

    Update devllo-events-woocommerce.php

--- devllo-events-woocommerce.php
+++ devllo-events-woocommerce.php
@@ -4,7 +4,7 @@
     Plugin URI: https://devlloplugins.com/
     Description: Purchase Events Tickets with WooCommerce
     Author: Devllo Plugins
-    Version: 0.2.3
+    Version: 0.3
     Author URI: https://devllo.com/
     Text Domain: devllo-events-woocommerce
     Domain Path: /languages
